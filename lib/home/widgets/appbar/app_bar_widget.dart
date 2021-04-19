04a838aa4e905f69ef4086649430859817c4f740
wasgton <wasgton.it@gmail.com>
2021-04-19 12:19:05 -0300

    Iniciando o projeto, SplashPage e appBar

---
+++ lib/home/widgets/appbar/app_bar_widget.dart
@@ -0,0 +1,55 @@
+import 'package:DevQuiz/home/widgets/scorecard/score_card_widget.dart';
+import '../../../core/core.dart';
+import 'package:flutter/material.dart';
+
+class AppBarWidget extends PreferredSize {
+  AppBarWidget(): super(
+    preferredSize: Size.fromHeight(250),
+      child:Container(
+        height: 250,
+        child: Stack(
+          children:[
+            Container(
+              height: 161,
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              width: double.maxFinite,
+              decoration: BoxDecoration(
+                  gradient: AppGradients.linear
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text.rich(
+                      TextSpan(
+                          text:"Olá, ",
+                          style: AppTextStyles.title,
+                          children: [
+                            TextSpan(
+                                text: "Dev",
+                                style: AppTextStyles.titleBold
+                            )
+                          ]
+                      )
+                  ),
+                  Container(
+                    height: 58,
+                    width: 58,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: DecorationImage(
+                            image: NetworkImage("https://media.gazetadopovo.com.br/viver-bem/2018/11/dog3-768x587-0e01be20.jpg")
+                        )
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Align(
+              alignment: Alignment(0.0,1.0),
+              child: ScoreCardWidget(),
+            )
+          ]
+        )
+    )
+  );
+}
---
+++ lib/home/widgets/scorecard/score_card_widget.dart
@@ -0,0 +1,44 @@
+import 'package:DevQuiz/home/widgets/chart/chart_widget.dart';
+import 'package:flutter/material.dart';
+import '../../../core/core.dart';
+
+class ScoreCardWidget extends StatelessWidget {
+
+  const ScoreCardWidget({Key?key}) : super(key:key);
+
+  @override
+  Widget build(BuildContext context){
+    return Padding(
+      padding: EdgeInsets.only(left: 20, right: 20, top: 14),
+      child: Container(
+      height: 136,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        color: AppColors.white,
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Expanded(
+            flex: 1, child: Center(child: ChartWidget())
+          ),
+          Expanded(
+            flex: 3,
+            child: Padding(
+              padding: EdgeInsets.only(left: 24),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                    Text("Vamos começar", style: AppTextStyles.heading,),
+                    Text("Complete os desafios e avance em conhecimento", style: AppTextStyles.body,)
+                ],
+              )
+            )
+          ),
+        ],
+      ),
+    ),
+    );
+  }
+}
